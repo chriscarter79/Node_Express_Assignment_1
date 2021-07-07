dd78094bbeceb399a06c1bd98e7734f6095db89c
chriscarter79 <chris.carter@outlook.com>
2021-07-07 14:51:21 -0400

    Completed /zoos/:all/ route, all tests passing

--- src/app.js
+++ src/app.js
@@ -9,27 +9,34 @@ const app = express();
 
 app.get("/check/:zip", validateZip, (req, res, next) => {
 	const { zip } = req.params;
-    if (!getZoos(zip)) {
+	if (!getZoos(zip)) {
 		res.send(`${zip} does not exist in our records.`);
 	} else {
 		res.send(`${zip} exists in our records.`);
 	}
 });
 
-app.get("/zoos/:zip", validateZip, (req, res, next) => {
-    const { zip } = req.params;
-    const zoos = getZoos(zip);
-    if(!zoos || !zoos.length){
-        res.send(`${zip} has no zoos.`)
-    } else {
-        res.send(`${zip} zoos: ${zoos.join("; ")}`)
+app.get("/zoos/all", (req, res) => {
+    const zoos = getZoos();
+	const admin = req.query.admin;
+    if (admin === "true") {
+		res.send(`All zoos: ${zoos.join("; ")}`);
+	} else {
+        res.send("You do not have access to that route.");
     }
-    
 });
 
-// app.get("/zoos/all", getZoos, (req, res, next) => {
+app.get("/zoos/:zip", validateZip, (req, res, next) => {
+	const { zip } = req.params;
+	const zoos = getZoos(zip);
+	if (!zoos || !zoos.length) {
+		res.send(`${zip} has no zoos.`);
+	} else {
+		res.send(`${zip} zoos: ${zoos.join("; ")}`);
+	}
+});
 
-// });
+
 
 app.use((req, res, next) => {
 	res.send(`That route could not be found!`);
