be27243b7f74a15327a315d4b150987ee9d214fd
chriscarter79 <chris.carter@outlook.com>
2021-07-07 13:38:10 -0400

    First commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const validateZip = require("./middleware/validateZip");
+const getZoos = require("./utils/getZoos");
+// const morgan = require("morgan");
+
+const app = express();
+
+// app.use(morgan("dev"));
+
+app.get("/check/:zip", validateZip, (req, res, next) => {
+	const { zip } = req.params;
+    if (!getZoos(zip)) {
+		res.send(`${zip} does not exist in our records.`);
+	} else {
+		res.send(`${zip} exists in our records.`);
+	}
+});
+
+app.get("/zoos/:zip", validateZip, (req, res, next) => {
+    const { zip } = req.params;
+    res.send(getZoos(zip));
+});
+
+// app.get("/zoos/all", getZoos, (req, res, next) => {
+
+// });
+
+app.use((req, res, next) => {
+	res.send(`That route could not be found!`);
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.send(err);
+});
+
+module.exports = app;
---
+++ src/middleware/validateZip.js
@@ -0,0 +1,13 @@
+const getZoos = require("../utils/getZoos");
+
+function validateZip(req, res, next) {
+	const zip = req.params.zip;
+    if (!getZoos(zip) && isNaN(zip) || zip.toString().length !== 5) {
+		// console.log(zip);
+		next(`Zip (${zip}) is invalid!`);
+	} else {
+		next();
+	}
+}
+
+module.exports = validateZip;
